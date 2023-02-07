ee2a83026e67592dbee8db91e85788927b6b2f52
Janwry Dela Cruz <janwry.jrv@gmail.com>
2023-02-07 21:27:42 +0800

    change class to classname on login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,11 +4,11 @@ function Login(){
         <div className='login-container'>
         <div className="login-box">
             <form>
-                <div class="user-box">
+                <div className="user-box">
                 <input required="" name="" type="text"/>
                 <label>Username</label>
                 </div>
-                <div class="user-box">
+                <div className="user-box">
                 <input required="" name="" type="password"/>
                 <label>Password</label>
                 </div><center>
