bd8ec17b3be237817b5f4c7f3519f7d7aab3f33a
Janwry Dela Cruz <janwry.jrv@gmail.com>
2023-02-09 19:30:21 +0800

    change info in text container at home section

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -13,8 +13,8 @@ function Home(){
             <h1 className="display-6 fw-bold mb-3 ">
                 ANIMAL HEALTHCARE MANAGEMENT SYSTEM 
             </h1>
-            <p className="lead">Incididunt laborum minim elit pariatur officia velit irure nostrud ullamco voluptate in cupidatat sunt. Laboris laborum ea nulla veniam. Lorem dolore velit duis nostrud sint pariatur sunt esse labore laboris nulla nostrud sint consectetur.</p>
-            <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+            <p className="lead">Modern zoos and aquaria have a top priority of ensuring high standards of animal welfare to support and promote wildlife conservation. The study aims to develop a management system to monitor and help animals with health problems or injuries by providing medical care. The objective is to design a system for staff members to access animal records and monitor the animals' health status. The system will improve overall animal welfare and contribute to the goals of the organization.</p>
+            <div className="d-grid gap-2 d-md-flex justify-content-md-start ">
               <Link to="/login">
               <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">Login</button>
               </Link>
