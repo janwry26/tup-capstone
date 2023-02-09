e016bcd72c12907cc01d3d15608b5bba2e4ab8a9
jbenedicto20.jbb@gmail.com <JBenedicto13>
2023-02-09 15:37:00 +0800

    Integrate login and logout function, configured routes and add private routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,30 @@
+import jwtDecode from 'jwt-decode';
+import setAuthToken from "./utils/setAuthToken";
+
 import './App.css';
 import Navbar from './components/Navbar.js';
-import Home from './components/Home.js';
-import Login from './components/Login.js';
-import {BrowserRouter,Routes, Route} from 'react-router-dom';
+import Routing from "./components/routing/Routing";
+import {BrowserRouter} from 'react-router-dom';
+
+let logUser;
+if (localStorage.getItem("token")) {
+    const jwt = localStorage.getItem("token");
+    setAuthToken(jwt);
+    logUser = jwtDecode(jwt);
+}
 
 function App() {
+  const user = logUser;
+  console.log("User: ", user)
   return (
-   <div>
-   <Navbar/>
-   <BrowserRouter>
-   <Routes>
-    <Route path="/" element={<Home/>}/>
-    <Route path="/login" element={<Login/>}/>
-   </Routes>
-   </BrowserRouter>
-
-   </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Navbar/>
+        <div className='main'>
+          <Routing user={user} />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,19 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 import '../styles/login.css';
-function Login(){
+import http from '../utils/http';
+
+function Login({user}){
+    let navigate = useNavigate();
+
+    useEffect(() => {
+        if (user) {
+            navigate(-1);
+        }
+    },[user, navigate])
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    function validation(e) {
+        e.preventDefault();
+
+        if (username == "" || password == "") {
+            alert("No field must be empty")
+        } else {
+            handleSubmit()
+        }
+    }
+
+    const handleSubmit = async () => {
+        
+        await http.post("/auth", {
+            username,
+            password
+        })
+        .then((res) => {
+            localStorage.setItem("token", res.data)
+            window.location = '/dashboard'
+        })
+        .catch((err) => alert(err))
+    };
+
     return(
         <div className='login-container'>
         <div className="login-box">
             <form>
                 <div className="user-box">
-                <input required="" name="" type="text"/>
-                <label>Username</label>
+                    <input
+                        required="" 
+                        name=""
+                        type="text"
+                        value={username}
+                        onChange={(e)=>setUsername(e.target.value)}
+                        />
+                    <label>Username</label>
                 </div>
                 <div className="user-box">
-                <input required="" name="" type="password"/>
-                <label>Password</label>
+                    <input 
+                        required="" 
+                        name="" 
+                        type="password"
+                        value={password}
+                        onChange={(e)=>setPassword(e.target.value)}
+                        />
+                    <label>Password</label>
                 </div><center>
-               <button className='btn'>
-                Sign in
+                <button className='btn' onClick={validation}>
+                    Sign in
                 </button></center>
             </form>
         </div>
