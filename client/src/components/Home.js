62ea95735d5316cd3527f33303167620c739322a
Janwry Dela Cruz <janwry.jrv@gmail.com>
2023-02-13 16:46:46 +0800

    enhance design on landing page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,20 +4,23 @@ import { Link } from 'react-router-dom';
 function Home(){
     return(
         <div className='hero-section'>
-        <div className="container col-xxl-8 px-1 py-5">
+        <div className="container col-xxl-8 px-2 py-5">
         <div className="row flex-lg-row-reverse align-items-center justify-content-center g-5 py-5">
           <div className="col-10 col-sm-8 col-lg-6 ">
             <img src={hero} className="d-block mx-lg-auto hero-img img-fluid" alt="Bootstrap Themes" width="600" height="400" />
           </div>
-          <div className="col-lg-6">
-            <h1 className="display-6 fw-bold mb-3 ">
+          <div className="col-lg-6 home-text-container">
+            <h1 className="title fw-bold mb-2 ">
                 ANIMAL HEALTHCARE MANAGEMENT SYSTEM 
             </h1>
-            <p className="lead">Modern zoos and aquaria have a top priority of ensuring high standards of animal welfare to support and promote wildlife conservation. The study aims to develop a management system to monitor and help animals with health problems or injuries by providing medical care. The objective is to design a system for staff members to access animal records and monitor the animals' health status. The system will improve overall animal welfare and contribute to the goals of the organization.</p>
+            <p className="lead">Zoos and aquaria today focus on providing top-notch care for the animals under their care. They're working on a new management system to keep tabs on the animals' health, so they can quickly identify and treat any problems. The goal is to make sure the animals are healthy and happy, and to support the larger mission of conservation.</p>
             <div className="d-grid gap-2 d-md-flex justify-content-md-start ">
               <Link to="/login">
-              <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">Login</button>
+              <button type="button" className="btn btn2 btn-primary btn-lg me-md-1">Login</button>
               </Link>
+              <a href='mailto:janwryd@gmail.com'>
+              <button type="button" className="btn btn1 btn-primary btn-lg  me-md-1">Contact Admin</button>
+              </a>
             </div>
           </div>
         </div>
