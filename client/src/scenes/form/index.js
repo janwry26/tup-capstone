3ad9a53dde3181ac9cdfdbb8a4339a952be651a5
Janwry Dela Cruz <janwry.jrv@gmail.com>
2023-05-04 20:14:01 +0800

    add passowrd in the form

--- client/src/scenes/form/index.js
+++ client/src/scenes/form/index.js
@@ -91,31 +91,31 @@ const Form = () => {
                 sx={{ gridColumn: "span 4" }}
               />
               <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Address 1"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.address1}
-                name="address1"
-                error={!!touched.address1 && !!errors.address1}
-                helperText={touched.address1 && errors.address1}
-                sx={{ gridColumn: "span 4" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Address 2"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.address2}
-                name="address2"
-                error={!!touched.address2 && !!errors.address2}
-                helperText={touched.address2 && errors.address2}
-                sx={{ gridColumn: "span 4" }}
-              />
+              fullWidth
+              variant="filled"
+              type="password"
+              label="Password"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.password}
+              name="password"
+              error={!!touched.password && !!errors.password}
+              helperText={touched.password && errors.password}
+              sx={{ gridColumn: "span 4" }}
+            />
+  <TextField
+  fullWidth
+  variant="filled"
+  type="password"
+  label="Confirm Password"
+  onBlur={handleBlur}
+  onChange={handleChange}
+  value={values.confirmPassword}
+  name="confirmPassword"
+  error={!!touched.confirmPassword && !!errors.confirmPassword}
+  helperText={touched.confirmPassword && errors.confirmPassword}
+  sx={{ gridColumn: "span 4" }}
+/>
             </Box>
             <Box display="flex" justifyContent="end" mt="20px">
               <Button type="submit" color="secondary" variant="contained">
@@ -132,24 +132,24 @@ const Form = () => {
 const phoneRegExp =
   /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
 
-const checkoutSchema = yup.object().shape({
-  firstName: yup.string().required("required"),
-  lastName: yup.string().required("required"),
-  email: yup.string().email("invalid email").required("required"),
-  contact: yup
-    .string()
-    .matches(phoneRegExp, "Phone number is not valid")
-    .required("required"),
-  address1: yup.string().required("required"),
-  address2: yup.string().required("required"),
-});
+  const checkoutSchema = yup.object().shape({
+    firstName: yup.string().required("required"),
+    lastName: yup.string().required("required"),
+    email: yup.string().email("invalid email").required("required"),
+    contact: yup
+      .string()
+      .matches(phoneRegExp, "Phone number is not valid")
+      .required("required"),
+    password: yup.string().required("required").min(8, "Password must be at least 8 characters long"),
+    confirmPassword: yup.string().oneOf([yup.ref('password')], 'Passwords must match').required("required")
+  });
+  
 const initialValues = {
   firstName: "",
   lastName: "",
   email: "",
   contact: "",
-  address1: "",
-  address2: "",
+  password: "",
 };
 
 export default Form;
