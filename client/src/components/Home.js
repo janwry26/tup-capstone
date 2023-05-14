1b8b0c6780878d0b37972f63edf0ad827f6b0a32
Janwry Dela Cruz <janwry.jrv@gmail.com>
2023-05-14 19:26:08 +0800

    remove the navbar from landing page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,31 +1,64 @@
-import '../styles/home.css'
-import hero from '../assets/images/hero/hero.jpg'
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-function Home(){
-    return(
-        <div className='hero-section'>
-        <div className="container col-xxl-8 px-2 py-5">
-        <div className="row flex-lg-row-reverse align-items-center justify-content-center g-5 py-5">
-          <div className="col-10 col-sm-8 col-lg-6 ">
-            <img src={hero} className="d-block mx-lg-auto hero-img img-fluid" alt="Bootstrap Themes" width="600" height="400" />
-          </div>
-          <div className="col-lg-6 home-text-container">
-            <h1 className="title fw-bold mb-2 ">
-                ANIMAL HEALTHCARE MANAGEMENT SYSTEM 
-            </h1>
-            <p className="lead">Zoos and aquaria today focus on providing top-notch care for the animals under their care. They're working on a new management system to keep tabs on the animals' health, so they can quickly identify and treat any problems. The goal is to make sure the animals are healthy and happy, and to support the larger mission of conservation.</p>
-            <div className="d-grid gap-3 d-md-flex justify-content-md-center home-button">
-              <Link to="/login">
-              <button type="button" className="btn btn2 btn-primary btn-lg">Login</button>
-              </Link>
-              <a href='mailto:janwryd@gmail.com'>
-              <button type="button" className="btn btn1 btn-primary btn-lg">Contact Admin</button>
-              </a>
-            </div>
-          </div>
-        </div>
+import '../styles/home.css'
+import { SlArrowDown,SlArrowUp } from 'react-icons/sl';
+function Home() {
+  useEffect(() => {
+    const button1 = document.getElementById('btn1');
+    const button2 = document.getElementById('btn2');
+    const button3 = document.getElementById('btn3');
+    const page2 = document.getElementById('page2');
+    const page3 = document.getElementById('page3');
+    const page1 = document.getElementById('page1');
+
+    const handleButton1Click = () => {
+      page2.classList.remove('hidden1');
+      page2.scrollIntoView({ behavior: 'smooth' });
+    };
+
+    const handleButton2Click = () => {
+      page3.classList.remove('hidden2');
+      page3.scrollIntoView({ behavior: 'smooth' });
+    };
+
+    const handleButton3Click = () => {
+      page2.classList.add('hidden1');
+      page3.classList.add('hidden2');
+      page1.scrollIntoView({ behavior: 'smooth' });
+    };
+
+    if (button1) button1.addEventListener('click', handleButton1Click);
+    if (button2) button2.addEventListener('click', handleButton2Click);
+    if (button3) button3.addEventListener('click', handleButton3Click);
+
+    // Clean up event listeners on component unmount
+    return () => {
+      if (button1) button1.removeEventListener('click', handleButton1Click);
+      if (button2) button2.removeEventListener('click', handleButton2Click);
+      if (button3) button3.removeEventListener('click', handleButton3Click);
+    };
+  }, []);
+
+  return (
+    <div className="main-container">
+      <div id="page1" className="first-container transition">
+        <h1>first page</h1>
+        <button id="btn1"><SlArrowDown  className='first-page-icon'/></button>
       </div>
+      <div id="page2" className="second-container hidden1 transition">
+        <h1>second page</h1>
+        <button id="btn2"><SlArrowDown  className='first-page-icon'/></button>
       </div>
-    )
+      <div id="page3" className="third-container hidden2 transition">
+        <h1>third page</h1>
+        <button id="btn3"><SlArrowUp  className='first-page-icon'/></button>
+      </div>
+    </div>
+    // <Link to="/login">
+    //   <button type="button" className="btn btn2 btn-primary btn-lg">Login</button>
+    // </Link>
+  );
 }
+
 export default Home;
+ 
