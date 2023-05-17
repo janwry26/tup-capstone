5904810c53c175f8c37e892f88ee0b34dd452930
Janwry Dela Cruz <janwry.jrv@gmail.com>
2023-05-17 20:18:15 +0800

    added dialog when click view profile

--- client/src/scenes/global/Topbar.js
+++ client/src/scenes/global/Topbar.js
@@ -1,17 +1,24 @@
-import { Box, IconButton, useTheme, Menu, MenuItem } from "@mui/material";
+import { Box, IconButton, useTheme, Menu, MenuItem, Dialog, DialogTitle, DialogContent, DialogActions, Button, Typography, TextField } from "@mui/material";
 import { useContext, useState } from "react";
 import { ColorModeContext, tokens } from "../../theme";
 import InputBase from "@mui/material/InputBase";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchIcon from "@mui/icons-material/Search";
-import Dashboard from "../../components/Dashboard";
 
 const Topbar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
   const [anchorEl, setAnchorEl] = useState(null);
+  const [isProfileDialogOpen, setIsProfileDialogOpen] = useState(false);
+  const userProfile = {
+    username: "john_doe",
+    name: "John Doe",
+    email: "johndoe@example.com",
+    contact: "1234567890",
+    role: "User",
+  };
 
   const handleMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
@@ -24,6 +31,15 @@ const Topbar = () => {
   const handleLogout = () => {
     localStorage.removeItem("token");
     window.location = "/login";
+  };
+
+  const handleViewProfile = () => {
+    setIsProfileDialogOpen(true);
+    handleMenuClose();
+  };
+
+  const handleCloseProfileDialog = () => {
+    setIsProfileDialogOpen(false);
   };
 
   return (
@@ -42,15 +58,8 @@ const Topbar = () => {
 
       {/* ICONS */}
       <Box display="flex">
-        {/* <IconButton onClick={colorMode.toggleColorMode}>
-          {theme.palette.mode === "dark" ? (
-            <DarkModeOutlinedIcon />
-          ) : (
-            <LightModeOutlinedIcon />
-          )}
-        </IconButton> */}
         <IconButton>
-        <NotificationsOutlinedIcon />
+          <NotificationsOutlinedIcon />
         </IconButton>
         <IconButton onClick={handleMenuOpen}>
           <PersonOutlinedIcon />
@@ -60,10 +69,65 @@ const Topbar = () => {
           open={Boolean(anchorEl)}
           onClose={handleMenuClose}
         >
-          <MenuItem onClick={handleMenuClose}>View Profile</MenuItem>
+          <MenuItem onClick={handleViewProfile}>View Profile</MenuItem>
           <MenuItem onClick={handleLogout}>Logout</MenuItem>
         </Menu>
       </Box>
+
+      {/* PROFILE DIALOG */}
+      <Dialog open={isProfileDialogOpen} onClose={handleCloseProfileDialog}>
+        <DialogTitle>Profile</DialogTitle>
+        <DialogContent>
+          <Box>
+            <Typography variant="subtitle1">Username:</Typography>
+            <TextField
+              value={userProfile.username}
+              fullWidth
+              variant="outlined"
+              disabled
+            />
+          </Box>
+          <Box>
+            <Typography variant="subtitle1">Name:</Typography>
+            <TextField
+              value={userProfile.name}
+              fullWidth
+              variant="outlined"
+              disabled
+            />
+          </Box>
+          <Box>
+            <Typography variant="subtitle1">Email:</Typography>
+            <TextField
+              value={userProfile.email}
+              fullWidth
+              variant="outlined"
+              disabled
+            />
+          </Box>
+          <Box>
+            <Typography variant="subtitle1">Contact:</Typography>
+            <TextField
+              value={userProfile.contact}
+              fullWidth
+              variant="outlined"
+              disabled
+            />
+          </Box>
+          <Box>
+            <Typography variant="subtitle1">Role:</Typography>
+            <TextField
+              value={userProfile.role}
+              fullWidth
+              variant="outlined"
+              disabled
+            />
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="warning" onClick={handleCloseProfileDialog}>Close</Button>
+        </DialogActions>
+      </Dialog>
     </Box>
   );
 };
